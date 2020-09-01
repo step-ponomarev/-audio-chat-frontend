de7b7dc40bbb6f1c0b2539f15944657678eb4044
Ponomarev Stepan <htconesuper11@gmail.com>
2020-09-01 05:19:31 +0300

    before typescript

--- src/components/Room.vue
+++ src/components/Room.vue
@@ -1,13 +1,10 @@
 <template>
 <div>
     <Notifications/>
-    <button :style="isSayMode" @click="say">
-        {{ sayMode ? `Say mode` : `Silence mode`}}
-    </button>
-
-    <ui>
+    <div>
+        Guest list:
         <li v-for="(guest, index) in guestList" :key="index"> {{ guest.name }}</li>
-    </ui>
+    </div>
 </div>
 </template>
 
@@ -19,10 +16,8 @@ import Notifications from "@/components/notifications/Notifications";
 export default {
   name: "Room",
   components: { Notifications },
-  data: () => ({
-    sayMode: false,
-    media: null
-  }),
+  data: () => ({}),
+  //TODO: Возможно имеет смысл сделать watch за роутом room вместо маунтед, но как поступать с дисконнектом?
   async mounted() {
     try {
       webSocketService.init();
@@ -31,6 +26,7 @@ export default {
       await this.fetchRoom(this.roomId);
       await this.fetchGuests(this.roomId);
     } catch (e) {
+      //TODO: Сделать нотификацию тип
       console.error('Failure fetching room');
     }
   },
@@ -40,25 +36,8 @@ export default {
   methods: {
     ...mapActions('room', ['fetchRoom']),
     ...mapActions('guest', ['removeGuest', 'addGuest', 'setUser', 'fetchGuests']),
-    onSocketSuccessfulConnected() {
-
-    },
     onSocketErrorConnected() {
       console.error('FAILURE SOCKET CONNECTION');
-    },
-    async say() {
-      this.sayMode = !this.sayMode;
-
-      if (this.sayMode) {
-        const media = await navigator.mediaDevices.getUserMedia({ audio: true });
-
-        const stream = new MediaStream();
-        media.getTracks().forEach(track => stream.addTrack(track, media));
-
-        webSocketService.stompClient.send(`/app/room/${ this.roomId }/say`, {}, "kek");
-      } else {
-        this.media = null;
-      }
     }
   },
   computed: {
@@ -66,9 +45,6 @@ export default {
     ...mapGetters('guest', ['guestList']),
     roomId() {
       return this.$route.params.id;
-    },
-    isSayMode() {
-      return this.sayMode ? 'color: green;' : 'color: red';
     }
   },
 }
